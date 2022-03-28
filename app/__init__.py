ff40066bf7f86a07ffd22dc668d2df2b382ccf25
ZanLaptopUbuntuApp <zander.gay@sjsu.edu>
2022-03-28 14:30:45 -0700

    removed alchemy library

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,9 +7,6 @@ myapp_obj = Flask(__name__)
 
 myapp_obj.config.from_mapping(
     SECRET_KEY = 'you-will-never-guess',
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 )
-
-db = SQLAlchemy(myapp_obj)
 
 from app import routes
