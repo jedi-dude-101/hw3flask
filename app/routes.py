a90b17f1de99c9d7979b80825ffdbd8893b83b3e
ZanLaptopUbuntuApp <zander.gay@sjsu.edu>
2022-03-30 17:22:17 -0700

    changed to flash method of posting

--- app/routes.py
+++ app/routes.py
@@ -12,11 +12,9 @@ name='Lisa'
 
 @myobj.route('/', methods=['GET','POST'])
 def home():
-	city_local=None
 	city_names=["Paris","London","Rome","Tahiti"]
 	form=LoginForm()
 	if form.validate_on_submit():
-		city_local=form.city.data
-		form.city.data=''
-	return render_template('home.html',name=name,city_names=city_names,form=form,city_local=city_local)
+		flash(form.city.data)
+	return render_template('home.html',name=name,city_names=city_names,form=form)
 
